4011ec2bc59a0544c6ccd57f5b11035305015c86
Donovan <donovanxcr@gmail.com>
2024-09-10 09:46:16 -0600

    No se que hacer

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
+class Examen(nombres: Array<String>){
+    val RespuestasCorrectas: CharArray = charArrayOf('a','c','b','a','d','b','b','c','a','a','b','d')
+    val notas: FloatArray = FloatArray(4)
+    var contadorExam: Int = 0
+
+    val respuestasEstudiante = arrayOf(
+        charArrayOf('a','c','b','a','d','b','b','c','a','a','b','d'),
+        charArrayOf('b','c','b','d','d','b','b','a','b','d','b','d'),
+        charArrayOf('c','c','b','a','a','b','c','c','a','a','b','c'),
+        charArrayOf('a','c','b','a','d','b','b','c','a','a','b','c')
+    )
+
+    fun leerRespuestas(){
+
+
+    }
+}
+
 fun main() {
-    println("Hello World!")
+val c = Examen(arrayOf("Marta", "Pedro", "Juan", "Maria"))
+
 }
