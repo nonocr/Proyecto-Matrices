ffd7fc08106607c3b8dfd77fcd13076dee651ce8
Donovan <donovanxcr@gmail.com>
2024-09-24 10:37:49 -0600

    ya avance mas

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,47 @@
-class Examen(nombres: Array<String>){
-    val RespuestasCorrectas: CharArray = charArrayOf('a','c','b','a','d','b','b','c','a','a','b','d')
-    val notas: FloatArray = FloatArray(4)
-    var contadorExam: Int = 0
-
-    val respuestasEstudiante = arrayOf(
-        charArrayOf('a','c','b','a','d','b','b','c','a','a','b','d'),
-        charArrayOf('b','c','b','d','d','b','b','a','b','d','b','d'),
-        charArrayOf('c','c','b','a','a','b','c','c','a','a','b','c'),
-        charArrayOf('a','c','b','a','d','b','b','c','a','a','b','c')
-    )
-
-    fun leerRespuestas(){
+class Examen( val nombres: Array<String>){
+    val plantilla = charArrayOf('a','c','b','a','d','b','b','c','a','a','b','d')
+    val notas = FloatArray(4)
+    var contador = 0
+    val respuestas = Array(4) { CharArray(12) }
 
 
+    fun leerRespuestas(RespuestasEstudiante: CharArray){
+        if (contador < 4) {
+            respuestas[contador] = RespuestasEstudiante
+            contador++
     }
-}
 
-fun main() {
-val c = Examen(arrayOf("Marta", "Pedro", "Juan", "Maria"))
+    fun calcularNota(){
+        for (f in 0 until 4) {
+            var correctas = 0
+            for (c in 0 until 12) {
+                if (respuestas[f][c] == plantilla[c]) {
+                    correctas++
+                }
+                notas[f] = (correctas.toFloat() / 12 * 100)
+    }
 
-}
+    fun promedioGrupo(): Float{
+        return notas.average().toFloat()
+    }
+
+    fun mayorNota(){
+        var notaMayor = 0
+        for (i in 1 until notas.size) {
+            if (notas[i] > notas[notaMayor]) {
+                notaMayor = i
+    }
+            println(notaMayor)
+
+    fun toString(){
+        for (i in 0 until 4){
+            println("Estudiante: ${nombres[f]} Respuestas: ${respuestas[f]} Nota: ${notas[f]}")
+            when {
+                notas[i] >= 70 -> println("Aprobó")
+                notas[i] >= 60 -> println("Aplazó")
+            }
+        }
+    }
+        }}}}}}
+
+
